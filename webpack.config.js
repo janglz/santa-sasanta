1e60a988d6398d84f60eb0f2b0bf90c66562f900
Janglz <wpidish@gmail.com>
2021-12-20 22:03:31 +0300

    config: test

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle-[hash].js',
-    path: path.resolve(__dirname, '/public'),
+    path: path.resolve(__dirname, '/tmp/public'),
     publicPath: path.resolve(__dirname, '/dist')
   },
   devServer: {

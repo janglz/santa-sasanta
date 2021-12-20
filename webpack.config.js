bb41bbc523fb84ffd3800b0bf8a6ee29529e1e40
Janglz <wpidish@gmail.com>
2021-12-21 00:21:28 +0300

    add tmp

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle-[hash].js',
-    path: path.resolve(__dirname, '/public')
+    path: path.resolve(__dirname, '/tmp/public')
   },
   devServer: {
     port: 8000,

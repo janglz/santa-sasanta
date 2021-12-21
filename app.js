547c4c594fdfa1a37fb9ae7c9d06cc5d1ea482d1
Janglz <wpidish@gmail.com>
2021-12-21 13:23:54 +0300

    fis app

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static(path.join(__dirname, 'dist')));
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + 'dist/index.html'));
 });
 
 app.listen(process.env.PORT || 1337, '0.0.0.0');

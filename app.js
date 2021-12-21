907f4d3591a53e4238d2d8591d4316d8b55d314b
Janglz <wpidish@gmail.com>
2021-12-21 23:02:00 +0300

    change path

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(__dirname + '/dist/santa-sasanta'));
+app.use(express.static(__dirname + '/'));
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '/santa-sasanta/index.html'));
